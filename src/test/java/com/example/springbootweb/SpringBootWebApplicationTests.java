b9785aa8c65629e0b0a051ec9ea3b6c546d3810c
liuha <loj897637)(>
2018-09-26 23:50:51 +0800

    添加redis配置

--- src/test/java/com/example/springbootweb/SpringBootWebApplicationTests.java
+++ src/test/java/com/example/springbootweb/SpringBootWebApplicationTests.java
@@ -1,16 +1,30 @@
 package com.example.springbootweb;
 
-import org.junit.Test;
+import com.example.springbootweb.model.Test;
+import com.example.springbootweb.service.TestService;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import javax.annotation.Resource;
+import java.util.List;
+
+
+//@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringBootWebApplication.class)
 public class SpringBootWebApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Resource
+    private TestService testService;
+
+    @org.junit.Test
+    public void testFindAll(){
+        List<Test> list = testService.findAll();
+        for (int i=0; i<list.size(); i++){
+            System.out.println("id:"+list.get(i).getId()+",name:"+list.get(i).getName());
+        }
+        System.out.println("size:"+list.size());
     }
 
 }
