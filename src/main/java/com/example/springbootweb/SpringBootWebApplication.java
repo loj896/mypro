965f8677161a66547fd4eb2f5af467373dd7017c
liuha <loj897637)(>
2018-09-26 23:49:28 +0800

    添加redis配置

--- src/main/java/com/example/springbootweb/SpringBootWebApplication.java
+++ src/main/java/com/example/springbootweb/SpringBootWebApplication.java
@@ -2,10 +2,13 @@ package com.example.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 //@MapperScan("com.example.springbootweb.mapper") 该注解和 @Mapper（用于Mapper接口） 二选一
 //@EnableScheduling 用于定时任务
+//开启Redis缓存
+@EnableCaching
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
